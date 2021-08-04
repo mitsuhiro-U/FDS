ee06e030dff3bdb9ed41a23e1ca4bb742073a846
mitsuhiro-U <mmm.udagawa@gmail.com>
2021-08-04 14:41:51 +0000

    for delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  sessions:      'job_seeker/sessions',
+  passwords:     'job_seeker/passwords',
+  registrations: 'job_seeker/registrations'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
