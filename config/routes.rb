dc99d45e2dc30b9a9cdfaaad1a4239d10033eb9a
mitsuhiro-U <mmm.udagawa@gmail.com>
2021-08-05 12:24:07 +0000

    [edit]registrations new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+ devise_for :users, controllers: {
+  sessions:      'user/sessions',
+  passwords:     'user/passwords',
+  registrations: 'user/registrations'
+}
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
