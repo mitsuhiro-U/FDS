f226ab8945b0c5c710bfe9217826d8dab5d812ad
mitsuhiro-U <mmm.udagawa@gmail.com>
2021-08-10 06:12:39 +0000

    [edit]Enterprice registrations[new.html.erb] sessions[new.html.erb]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
 
+
+    def after_sign_in_path_for(resource)
+      case resource
+        when User
+          users_path
+        when Enterprise
+          enterprise_enterprises_path
+        end
+    end
+
+    def after_sign_out_path_for(resource)
+        #binding.pry
+        case resource
+        when :user
+            top_path
+        when :enterprise
+            enterprise_top_path
+        end
+    end
+
 end
