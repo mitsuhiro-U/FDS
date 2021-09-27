82aeb6bbe2ae5b74565cb7265bca8b64d020056e
mitsuhiro-U <mmm.udagawa@gmail.com>
2021-09-27 15:01:00 +0000

    [edit]valitaion

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,8 @@ class User < ApplicationRecord
   #------- バリテーション関連------------------------------------------------------------------------------
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   validates :birthday, presence: true
   validates :gender, presence: true
 
